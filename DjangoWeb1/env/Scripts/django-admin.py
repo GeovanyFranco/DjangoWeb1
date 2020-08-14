22695cfa664066e21f9c01e08e93e70a964c98cc
GeovanyFranco <vfrancos@miumg.edu.gt>
2020-08-14 15:51:05 -0600

    Agregar archivos de proyecto.

---
+++ DjangoWeb1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\geofr\source\repos\DjangoWeb1\DjangoWeb1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
